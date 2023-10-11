8cf436d821549e9d06151cd5ad5457ec6bebcbed
mkoroleva5 <mkoroleva5@gmail.com>
2023-10-11 18:35:58 +0300

    feat: add header profile and cart buttons

---
+++ components/elements/HeaderCart.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="cart-button">
+    <img src="~/assets/images/svg/bag.svg" />
+  </button>
+</template>
+
+<style scoped lang="scss">
+.cart-button {
+  width: fit-content;
+  height: fit-content;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
