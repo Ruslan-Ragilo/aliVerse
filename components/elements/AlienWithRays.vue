8eca44c5aac8f4dd6a27c4df1277c2cff1275cac
mkoroleva5 <mkoroleva5@gmail.com>
2023-10-11 18:36:39 +0300

    feat: add svg element of alien with red rays

---
+++ components/elements/AlienWithRays.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-rays">
+      <img class="rays" src="@/assets/images/svg/redRays.svg" alt="" />
+      <div class="wrapper-alien">
+        <img class="alien" src="@/assets/images/svg/alien.svg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper-rays {
+  width: 100%;
+  position: relative;
+}
+.wrapper-alien {
+  width: 30%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.rays {
+  user-select: none;
+  width: 100%;
+}
+.alien {
+  transform: rotate(15deg);
+  user-select: none;
+}
+</style>
