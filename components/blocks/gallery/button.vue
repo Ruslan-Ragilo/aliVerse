05eb9231c8807f8a6550d328cb8d2b18a35e6657
mkoroleva5 <mkoroleva5@gmail.com>
2023-10-11 18:39:17 +0300

    feat: add slider and button to gallery

---
+++ components/blocks/gallery/button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper-button">
+    <ElementsButtonRegedtrtion />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper-button {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 35px 70px;
+  margin-bottom: 70px;
+}
+</style>
