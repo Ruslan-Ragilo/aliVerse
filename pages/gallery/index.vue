3d592ad01b9b1e5c65beba5bf32efef96eddf8ac
mkoroleva5 <mkoroleva5@gmail.com>
2023-10-11 18:37:43 +0300

    feat: add gallery title and header

---
+++ pages/gallery/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLayout>
+    <div class="wrapper-gallery">
+      <BlocksGalleryHeader />
+      <BlocksGalleryTitle />
+      <BlocksGalleryPhotos />
+      <BlocksGalleryButton />
+    </div>
+  </NuxtLayout>
+</template>
+
+<style scoped lang="scss">
+.wrapper-gallery {
+  width: 100%;
+  min-height: 100vh;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
