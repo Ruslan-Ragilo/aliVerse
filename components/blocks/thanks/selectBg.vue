b0ea6f6605ea6428ed2f11af8c0c2bafcdc55413
mkoroleva5 <mkoroleva5@gmail.com>
2023-10-13 15:04:12 +0300

    feat: add thanks modal window

---
+++ components/blocks/thanks/selectBg.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper-select" :class="selectedBg ? `light` : ``">
+    <ElementsText transform="upper">Выбери фон</ElementsText>
+    <div class="wrapper-inputs">
+      <ElementsRadioInput
+        v-model="selectedBg"
+        value="bg1"
+        @update:model-value="onBgChange"
+      >
+        <img src="~/assets/images/png/thanks/bg1.png" alt="" />
+      </ElementsRadioInput>
+      <ElementsRadioInput
+        v-model="selectedBg"
+        value="bg2"
+        @update:model-value="onBgChange"
+      >
+        <img src="~/assets/images/png/thanks/bg2.png" alt="" />
+      </ElementsRadioInput>
+      <ElementsRadioInput
+        v-model="selectedBg"
+        value="bg3"
+        @update:model-value="onBgChange"
+      >
+        <img src="~/assets/images/png/thanks/bg3.png" alt="" />
+      </ElementsRadioInput>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  onBgChange: (value: string) => void;
+}>();
+const selectedBg = ref(null);
+</script>
+
+<style scoped lang="scss">
+.wrapper-select {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  @include media(450px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+}
+
+.wrapper-inputs {
+  display: flex;
+  gap: 10px;
+}
+
+.wrapper-select p {
+  color: #bdbdbd;
+}
+
+.light p {
+  color: rgba(255, 255, 255, 0.5);
+}
+</style>
