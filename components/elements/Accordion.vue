c736b1dd24521a2d02dbd978180c0899b31c3d82
mkoroleva5 <mkoroleva5@gmail.com>
2023-10-19 13:54:28 +0300

    feat: add data to FAQ block

--- components/elements/Accordion.vue
+++ components/elements/Accordion.vue
@@ -3,25 +3,19 @@ const isActive = ref(false);
 </script>
 
 <template>
-  <div @click="isActive = !isActive" class="wrapper-item">
+  <div class="wrapper-item" @click="isActive = !isActive">
     <img
       src="@/assets/images/svg/caret.svg"
       :class="['caret', { active: isActive }]"
       alt=""
     />
-    <ElementsText class="question" transform="upper" size="m"
-      >Как выиграть мерч?</ElementsText
-    >
+    <ElementsText class="question" transform="upper" size="m">
+      <slot name="question" />
+    </ElementsText>
     <div :class="['answers', { active: isActive }]">
-      <ElementsText transform="upper"
-        >Разнообразный и богатый опыт говорит нам, что современная методология
-        разработки способствует повышению качества глубокомысленных рассуждений.
-        В целом, конечно, современная методология разработки прекрасно подходит
-        для реализации как самодостаточных, так и внешне зависимых
-        концептуальных решений. Противоположная точка зрения подразумевает, что
-        независимые государства и по сей день остаются уделом
-        либералов.</ElementsText
-      >
+      <ElementsText transform="upper">
+        <slot name="answer" />
+      </ElementsText>
     </div>
   </div>
 </template>
@@ -33,6 +27,7 @@ const isActive = ref(false);
   background-color: $white;
   padding: 44px 44px 0 44px;
   position: relative;
+  cursor: pointer;
 
   .question {
     padding-bottom: 44px;
