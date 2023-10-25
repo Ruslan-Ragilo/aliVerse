e8b7f5330f40bc17f92b3c412bea90810501efd4
LeraVilar <69603826+LeraVilar@users.noreply.github.com>
2023-10-25 04:27:04 +0400

    finalFrog

---
+++ components/blocks/shadows/finish.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper-finish">
+    <ElementsModal :on-close="store.closeModal">
+      <template #message>
+        <div>{{ coins > 0 ? "Поздравляем" : "Ничего, повезет" }}</div>
+        <div>{{ coins > 0 ? "ты заработал" : "в следующий раз" }}</div>
+      </template>
+      <template #points>{{ coins }}</template>
+      <template #buttonText>{{ coins > 0 ? "Ура,спасибо!" : "Ок" }}</template>
+    </ElementsModal>
+  </div>
+</template>
+
+<script setup lang="ts">
+const store = useShadowsStore();
+const coins = ref(store.getCoins());
+</script>
+
+<style scoped lang="scss">
+.wrapper-finish {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+}
+</style>
