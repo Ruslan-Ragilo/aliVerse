045e2eb8a018ec8b3153867b30a20a042e8199c3
mkoroleva5 <mkoroleva5@gmail.com>
2023-11-07 18:10:18 +0300

    fix: fix frog availability

--- components/blocks/frog/welcome.vue
+++ components/blocks/frog/welcome.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="wrapper-welcome">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="64px" height="64px">
-      <path d="M 11 2 L 11 4 L 21 4 L 21 2 L 11 2 z M 21 4 L 21 6 L 23 6 L 23 4 L 21 4 z M 23 6 L 23 16 L 25 16 L 25 6 L 23 6 z M 23 16 L 21 16 L 21 18 L 23 18 L 23 16 z M 21 18 L 11 18 L 11 20 L 15 20 L 15 23 L 14 23 L 14 30 L 18 30 L 18 23 L 17 23 L 17 20 L 21 20 L 21 18 z M 11 18 L 11 16 L 9 16 L 9 18 L 11 18 z M 9 16 L 9 6 L 7 6 L 7 16 L 9 16 z M 9 6 L 11 6 L 11 4 L 9 4 L 9 6 z" fill="#FFF"/>
-    </svg>
-    <p>Прилближай картинку, <br />чтобы точно найти жабку</p>
-    <div class="start-button">
-      <ElementsPixelButton color="red" size="middle" @click="startGame">
-        Начать
+    <div v-if="isAvailable" class="message-wrapper">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32"
+        width="64px"
+        height="64px"
+      >
+        <path
+          d="M 11 2 L 11 4 L 21 4 L 21 2 L 11 2 z M 21 4 L 21 6 L 23 6 L 23 4 L 21 4 z M 23 6 L 23 16 L 25 16 L 25 6 L 23 6 z M 23 16 L 21 16 L 21 18 L 23 18 L 23 16 z M 21 18 L 11 18 L 11 20 L 15 20 L 15 23 L 14 23 L 14 30 L 18 30 L 18 23 L 17 23 L 17 20 L 21 20 L 21 18 z M 11 18 L 11 16 L 9 16 L 9 18 L 11 18 z M 9 16 L 9 6 L 7 6 L 7 16 L 9 16 z M 9 6 L 11 6 L 11 4 L 9 4 L 9 6 z"
+          fill="#FFF"
+        />
+      </svg>
+      <p>Прилближай картинку, <br />чтобы точно найти жабку</p>
+      <div class="start-button">
+        <ElementsPixelButton color="red" size="middle" @click="startGame">
+          Начать
+        </ElementsPixelButton>
+      </div>
+    </div>
+    <div v-if="!isAvailable" class="message-wrapper">
+      <div class="message">
+        Жабку ты сегодня уже нашел<br />
+        Возвращайся через неделю!
+      </div>
+      <ElementsPixelButton color="red" size="middle" @click="store.closeModal">
+        Вернуться
       </ElementsPixelButton>
     </div>
   </div>
@@ -14,18 +33,38 @@
 
 <script setup lang="ts">
 const store = useFrogStore();
-const resolution = window.innerWidth;
+const isAvailable = computed(() => store.isAvailable);
+// const resolution = window.innerWidth;
 
 onMounted(() => {
   store.checkAvailability();
 });
 
 const startGame = () => {
-  store.gameScreen = "rules";
+  store.gameScreen = "game";
 };
 </script>
 
 <style scoped lang="scss">
+.message-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.message {
+  max-width: 830px;
+  font-size: 20px;
+  line-height: 130.5%;
+  text-transform: uppercase;
+  margin-bottom: 30px;
+  text-align: center;
+  color: #fff;
+
+  @include media(500px) {
+    font-size: 14px;
+  }
+}
 .wrapper-welcome {
   position: absolute;
   width: 100%;
